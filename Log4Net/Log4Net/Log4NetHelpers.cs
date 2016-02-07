5b7d63857e1ad2b52f705bea37b4dbaa45f06343
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2016-02-07 18:37:45 +0200

    added 'log4net' demo project

---
+++ Log4Net/Log4Net/Log4NetHelpers.cs
@@ -0,0 +1,12 @@
+﻿namespace Log4Net
+{
+    using System.Runtime.CompilerServices;
+
+    public class Log4NetHelpers
+    {
+        public static log4net.ILog GetLogger([CallerFilePath]string filename = "")
+        {
+            return log4net.LogManager.GetLogger(filename);
+        }
+    }
+}
---
+++ Log4Net/Log4Net/Program.cs
@@ -0,0 +1,17 @@
+﻿[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Log4Net
+{
+    class Program
+    {
+        private static readonly log4net.ILog logger = Log4NetHelpers.GetLogger();
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world");
+
+            logger.Error("Error message", new System.OutOfMemoryException());
+
+            return;
+        }
+    }
+}
